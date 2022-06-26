acdce9118f502592525b8c023840e4e1aa12fe52
Stefan Stingl <stfnstingl@gmail.com>
2022-06-26 02:00:28 +0200

    Added KNN and Refactored

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -1,10 +1,7 @@
-import tensorflow
-import keras
 import pandas as pd
 import numpy as np
 import sklearn
-from sklearn import linear_model
-from sklearn.utils import shuffle
+import sklearn.model_selection
 import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
