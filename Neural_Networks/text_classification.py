32ffe8ca4528d777facd8d91c181364473851f30
Stefan Stingl <stfnstingl@gmail.com>
2022-07-08 19:06:56 +0200

    Added real world examples and finished rly simple NN with rly simple Embedding

--- Neural_Networks/text_classification.py
+++ Neural_Networks/text_classification.py
@@ -2,9 +2,10 @@ import tensorflow as tf
 import tensorflow.keras as keras #Fixes code suggestions
 import numpy as np
 
+
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 print(train_data[0])
 
@@ -26,16 +27,26 @@ reversed_word_index = dict([(value,key) for (key, value) in word_index.items()])
 train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], padding="post", maxlen=250)
 
-print(len(test_data[0]), len(test_data[1])) # Doesnt work for our Model, need to be same length for Input Neurons
+# print(len(test_data[0]), len(test_data[1])) # Doesnt work for our Model, need to be same length for Input Neurons
 
 
 def decode_review(text):
     return " ".join([reversed_word_index.get(i, "?") for i in text]) #Return all of keys we want (human readable words)
 
-#Model
+def review_encode(s):
+    encoded = [1]
 
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encoded
+#Model
+"""
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
+model.add(keras.layers.Embedding(88000, 16))
 # Embedding layer tries to group words in a similair way (finds word vectors) 16 dimensional here (10000 Word vectors)
 model.add(keras.layers.GlobalAveragePooling1D())
 # Takes whatever dimension our Data is in and puts it in another Dimension
@@ -61,4 +72,28 @@ fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_dat
 results = model.evaluate(test_data, test_labels)
 
 print(results)
+
+#Saving Model
+
+model.save("model.h5") # h5 extension for saved model in keras/tensorflow, saves model in binary data
+"""
+
+#Load model
+model = keras.models.load_model("model.h5")
+"""Realworld_Review_IMDB_In_Time_10of10Stars.txt"""
+#Handles Lion King Review well, but more complicated like the IMDB Review of Just-in-Time not so well.
+#Probably because embeddings are too simple. Maybe with more sophisticated like Bert Embeddings it will be better
+#Test model on real world data
+with open("test.txt", encoding="utf-8") as file:
+    for line in file.readlines():
+        #This should work better with stopword library and nltk or smth. but easy way in tutorial w/o library so i do it too
+        #If we Embed Words we only want words, bc we split by spaces. So we get "Word" and not "Word." or "Word,"
+        nline = line.replace(",", "").replace(".", "").replace("(","").replace(")", "").replace(":", "").replace("\"", "").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post",
+                                                                maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
 
